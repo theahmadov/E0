0280d32946d499696052afa8ecdec665d476098f
Error <88781785+TheSadError@users.noreply.github.com>
2022-05-07 00:31:25 +0400

    Update E0.py

--- E0.py
+++ E0.py
@@ -2,14 +2,19 @@ import os
 import time
 from colorama import Fore,Back,Style
 from rich.progress import track
-import re
 import E1 as E0
+import requests
+import re
+from bs4 import BeautifulSoup
+import csv
+import logging
+import cv2 
 
 def loading(txt,color):
     for _ in track(range(100), description=f'[{color}]{txt}'):
         time.sleep(0.01)
 
-def message_probability(um, rw, schat=False, req=[]):
+def probability(um, rw, schat=False, req=[]):
     count = 0
     hw = True
     for w in um:
@@ -26,11 +31,44 @@ def message_probability(um, rw, schat=False, req=[]):
         return 0
 
 
+
+def menu():
+    os.system("clear")
+    print(Fore.YELLOW+"""
+              
+            -_________________-_________________-
+            |                                   |
+            |  |-_-| Welcome to E0 Bot!  |-_-|  |
+            -_________________-_________________-
+            
+    """)
+    print("[1] Face Dedection")
+    print("")
+    n = input(Fore.RED+"Please select : ")
+    if n=="1":
+        os.system("python3 EFace.py")
+
+
+def help():
+    os.system("clear")
+    print(Fore.YELLOW+"""
+              
+            -_________________-_________________-
+            |                                   |
+            |  |-_-| Welcome to E0 Bot!  |-_-|  |
+            -_________________-_________________-
+            
+    """)
+    print(Fore.YELLOW+"help  : To List Commands")
+    print(Fore.YELLOW+"menu  : To Print Menu (Other Functions Of Bot)")
+    print(Fore.YELLOW+"clear : To Clear Chat")
+    print(Fore.YELLOW+"exit  : To Exit")
+
 def check_all_messages(message):
     high = {}
     def dns(bchat, lw, schat=False, req=[]):
         nonlocal high
-        high[bchat] = message_probability(message, lw, schat, req)
+        high[bchat] = probability(message, lw, schat, req)
 
     dns('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo','hola','salam','merhaba'], schat=True)
     dns('See you!', ['bye', 'goodbye'], schat=True)
@@ -40,6 +78,9 @@ def check_all_messages(message):
     dns('Sure!',['want', 'to', 'be', 'my','friend'],schat=True)
     dns('My name is E0. And I created by Err0r. Err0r is python,cpp and ruby developer and competitive programmer.',['name', 'who', 'who are you', 'who are you', 'who are you?','are you?','are you'],schat=True)
     dns('I know python,c and java script languages.',['want', 'ruby', 'cpp', 'python','which language','do you know ?','know?','know','programming language'],schat=True)
+    dns('',["play?","game?",'what we can do?','what we can do','what we can','what we can?','what can you do?','what can you do','what can you?',"what can you"],schat=True)
+    dns('Yess...',['are you okey?','you okey?','you ok?','you okey'],schat=True)
+    dns('I am feeling bad.','I am sad. Hope you are better.',['feel?','how are you feel?','feelings?'],req='feel')
     dns(E0.R_ADVICE, ['give', 'advice'], req=['advice'])
     dns(E0.R_EATING, ['what', 'you', 'eat'], req=['you', 'eat'])
     dns(E0.R_GAME, ['what', 'game', 'you','play'], req=['game', 'play'])
@@ -62,7 +103,6 @@ def chatbot():
             |  |-_-| Welcome to E0 Bot!  |-_-|  |
             -_________________-_________________-
             
-
     """)
     
     loading("Loading...","yellow")
@@ -73,8 +113,10 @@ def chatbot():
             print(Fore.BLACK+"Exitting...")
             time.sleep(2)
             break
+        if(x == "list" or x == "help"):
+            help()
+        if(x == "menu"):
+            menu()
         elif(x == "clear"):
-            time.sleep(2)
-            os.system("clear")
-
+            chatbot()
 chatbot()
