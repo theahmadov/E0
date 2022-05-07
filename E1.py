0382d41d24781c1ff03660b27d8db2a35cd85d56
Error <88781785+TheSadError@users.noreply.github.com>
2022-05-07 15:02:09 +0400

    Update E1.py

--- E1.py
+++ E1.py
@@ -1,4 +1,10 @@
 import random
+import os
+import time
+from colorama import Fore,Back,Style
+import requests
+import re
+
 advicelist = ["You can Improve my code. Its so simple...","You can play a game...","You may learn new programming language...","You can star my github repo."]
 gamelist = ["Fortnite","Valorant","Far Cry 5","GTA V","Tomb Rider","Control","Far Cry 6","Snake Game","Football","Basketball"]
 R_EATING = "Ummm... You may eat pasta. But my favorite foods are : CPU and RAM. They are awsome. You must try!"
