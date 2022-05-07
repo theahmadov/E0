4c26c624523cdf7aa850e13c972e2a52f14ed2b9
Error <88781785+TheSadError@users.noreply.github.com>
2022-05-07 17:00:02 +0400

    Update E0.py

--- E0.py
+++ E0.py
@@ -10,6 +10,15 @@ import csv
 import logging
 import cv2 
 
+
+"""
+If you use windows please change
+os.system("clear")
+to 
+os.system("cls")
+than it will work
+"""
+
 def loading(txt,color):
     for _ in track(range(100), description=f'[{color}]{txt}'):
         time.sleep(0.01)
@@ -67,7 +76,30 @@ def help():
     print(Fore.YELLOW+"ls    : To List Files In Directory")
     print(Fore.YELLOW+"face  : To Run Face Dedection")
 
+def info():
+    print(Fore.BLUE+"""
+    
+            -_________________-_________________-
+            |                                   |
+            |  |-_-| Welcome to E0 Bot!  |-_-|  |
+            -_________________-_________________- 
 
+            Hi ? Are you there ? Ok... I will say 
+            story. I created by Error. His other 
+            name is TheSadError. He created my on 
+            5 may 2022 . He used python3.9 . And he 
+            added E1 packet for unknown things. It 
+            working awsomely. And I am open source. 
+            This means you can see my code and can edit
+            them for yourself. For this Error shared 
+            my code in his own github repo. Let me explain
+            E0 logic. Logic : 
+            User input a sentence or words probability
+            function searching for probability. Which words 
+            probability suitability is bigger in database
+            E0 prints answear of the word base. Its simple AI. 
+
+    """)
 
 
 
@@ -76,8 +108,8 @@ def check_all_messages(message):
     def dns(bchat, lw, schat=False, req=[]):
         nonlocal high
         high[bchat] = probability(message, lw, schat, req)
-
-    dns('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo','hola','salam','merhaba'], schat=True)
+    # Simple
+    dns('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo','hola','salam','merhaba','selam','السلام عليكم','عليكم','السلام'], schat=True)
     dns('See you!', ['bye', 'goodbye'], schat=True)
     dns('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], req=['how'])
     dns('You\'re welcome!', ['thank', 'thanks'], schat=True)
@@ -89,12 +121,15 @@ def check_all_messages(message):
     dns('Yess...',['are you okey?','you okey?','you ok?','you okey'],schat=True)
     dns('I am feeling bad.','I am sad. Hope you are better.',['feel?','how are you feel?','feelings?'],req='feel')
     dns('Ah. I understand. I live in Github Repo. Write "github" for see it. My Discord :err0r#4018 ',['social','social media','discord','media'],schat=True)
+    dns('Some Music Advices : Old Town Road , Montero , SR - Welcome To Brixton , 6 For 6',['music','musik','muzik'],schat=True)
     
-    dns('WOW', ['wow', 'awsome', 'good', 'cool'], schat=True)
+    dns('WOW', ['wow', 'awsome', 'good', 'good' , 'col' ,'cool'], schat=True)
     dns(E0.R_ADVICE, ['give', 'advice'], req=['advice'])
     dns(E0.R_EATING, ['what', 'you', 'eat'], req=['you', 'eat'])
     dns(E0.R_GAME, ['what', 'game', 'you','play'], req=['game', 'play'])
     
+
+    # People
     dns("""Elon Musk, aka Elon Reeve Musk, is an engineer, industrial designer, tech entrepreneur, 
 and philanthropist. Apart from his birthplace of South Africa, he is a Canadian and US citizen 
 and lives in the USA, where he immigrated at the age of 20.
@@ -110,6 +145,14 @@ Bezos, who was one of the first hundred billionaires on the Forbes wealth index 
 man in the world after 2017.
     """,['jeff','bezos','jeff bezos'],schat=True)
 
+
+    dns("""John Christopher Depp II is an American actor, producer and musician. Nominated for ten Golden Globe Awards, 
+he was nominated for an Academy Award for Best Actor for his role as 
+the Demon Barber in Sweeney Todd: The Demonic Barber of Fleet Street.
+    """,['johnny','Johnny','depp','Depp','Johnny Depp','johnny depp'],schat=True)
+
+
+
     bestm = max(high, key=high.get)
 
     return E0.unknown() if high[bestm] < 1 else bestm
@@ -119,7 +162,7 @@ def chat(user_input):
     dns = check_all_messages(sm)
     return dns
 
-def chatbot():
+def chatbot(name):
     os.system('clear')
     print(Fore.YELLOW+"""
               
@@ -131,15 +174,19 @@ def chatbot():
     """)
     
     loading("Loading...","yellow")
+    
     while True:
-        x = input(Fore.RED+'You : ')
-        print(Fore.YELLOW+'Bot : ' + chat(x) )
+
+        x = input(Fore.RED+f'[{name}] : ')
+        print(Fore.YELLOW+'[E0] : ' + chat(x) )
         if(x == "exit" or x == "quit" or x == 'break'):
             print(Fore.BLACK+"Exitting...")
             time.sleep(2)
             break
         if(x == "list" or x == "help"):
             help()
+        if(x == "info" or x == "e0"):
+            info()
         elif(x == "ls" or x == "LS"):
             os.system('ls')
         elif(x == "FACE" or x == "face"):
@@ -149,5 +196,17 @@ def chatbot():
         elif(x == "menu"):
             menu()
         elif(x == "clear"):
-            chatbot()
-chatbot()
+            chatbot(name)
+
+
+os.system("clear")
+print(Fore.YELLOW+"""
+          
+        -_________________-_________________-
+        |                                   |
+        |  |-_-| Welcome to E0 Bot!  |-_-|  | 
+        -_________________-_________________- 
+            
+""")
+name = input(Fore.MAGENTA+"Please input your name : ")
+chatbot(name)
